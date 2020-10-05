fc5527b84b96ad059a2de2184ebbea7e7df6f713
Yuri Baldan <yuribbp@gmail.com>
2020-10-05 20:26:25 -0300

    Including Alert button message

--- src/gui/ViewController.java
+++ src/gui/ViewController.java
@@ -1,7 +1,9 @@
 package gui;
 
+import gui.util.Alerts;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Alert.AlertType;
 
 public class ViewController {
 
@@ -10,6 +12,6 @@ public class ViewController {
 
 	@FXML
 	public void onBtTestAction() {
-		System.out.println("Click!");
+		Alerts.showAlert("Alert title", "Alert header", "Hello!", AlertType.INFORMATION);
 	}
 }
